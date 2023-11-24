8ede19c8a517f75f8a72157c5846031a9ded0e50
renegarcia15 <141770669+renegarcia15@users.noreply.github.com>
2023-11-24 12:41:21 -0800

    Base de datos final del proyecto

---
+++ BaseFinal_WEB.sql
@@ -0,0 +1,24 @@
+create database jugadores;
+use jugadores;
+
+create table jugador(
+	nombre varchar(50) NOT NULL PRIMARY KEY,
+    edad int, 
+    posicion varchar(60),
+    nacionalidad varchar(40));
+    
+create table equipos(
+	nombre varchar(50) NOT NULL,
+    equipo varchar (60),
+    foreign key (nombre) references jugador(nombre));
+    
+create table valores(
+	nombre varchar(50) NOT NULL,
+    valor_mercado_millones int(40) NOT NULL,
+    foreign key (nombre) references jugador (nombre));
+
+select * from jugador;
+select * from equipos;
+select * from valores;
+
+drop database jugadores
