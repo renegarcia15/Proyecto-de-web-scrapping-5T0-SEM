a918d241c6a63c62970cbe5b37a4ae6d68c0636f
joseguzman <jose.guzman.hernandez@uabc.edu.mx>
2023-12-04 22:32:51 -0800

    Issue 10

--- Dashboards.py
+++ Dashboards.py
@@ -99,7 +99,61 @@ def dashboard02():
     mejores_equipos = df_d2.groupby("equipo")["valor_mercado_millones"].sum().nlargest(10).reset_index()
     graf_mejores = px.bar(mejores_equipos, x="equipo", y="valor_mercado_millones",
                           title="Top 10 Equipos con el mejor valor del mercado").update_layout(yaxis_title="Valor en el mercado")
+    peores_equipos = df_d2.groupby("equipo")["valor_mercado_millones"].sum().nsmallest(5).reset_index()
 
+
+graf_peores = px.pie(peores_equipos, names="equipo", values="valor_mercado_millones",
+                     title="Top 10 Equipos con el peor valor en el mercado")
+return dbc.Container([
+    dbc.Row([html.P(
+        "Objetivos: Mostrar los equipos con más jugadores, la cantidad de jugadores por equipo y qué equipo tiene a más jugadores en el top 50")]),
+    dbc.Row([
+        dbc.Col([
+            dcc.Dropdown(
+                id="dropdown-equipos",
+                options=[{"label": equipo, "value": equipo} for equipo in df_d2["equipo"].unique()],
+                value=None,
+                multi=False,
+                placeholder="Selecciona equipo"
+            ),
+        ], width=6),
+        dbc.Col(dcc.Graph(id="grafica-equipos"))
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id="grafica-top-equipos", figure=graf_mejores))
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id="grafica-peores-equipos", figure=graf_peores))
+    ]),
+])
+
+app.layout = html.Div([
+    html.H2("Proyecto final Transfermarkt"),
+    html.P(
+        "Integrantes: Contreras Ramirez Juan Manuel, Garcia Soto Rene, Gutierrez Acosta Axel Michel, Guzman Hernandez Jose Iñaki, Rueda Franco Julian Fernando"),
+    dcc.Tabs([
+        dcc.Tab(label="Jugadores", children=[dashboard01()]),
+        dcc.Tab(label="Equipos", children=[dashboard02()]),
+    ])
+])
+
+
+@app.callback(
+    Output("grafica-por-pais", "figure"),
+    [Input("dropdown-paises", "value")]
+)
+def act_graficaPais(selected_pais):
+    query = cnst.CONSULTA_JUGADOR
+    query2 = cnst.CONSULTA_VALORES
+    df_jugador, df_valores = conexion.obtener_datos(query, query2)
+    df = pd.merge(df_jugador, df_valores)
+    if not selected_pais:
+        filtered_df = df
+    else:
+        filtered_df = df[df["nacionalidad"] == selected_pais]
+    figure = px.bar(filtered_df, x="nacionalidad", title="Jugadores por País").update_layout(
+        yaxis_title="Cantidad de jugadores")
+    return figure
 
 if __name__ == "__main__":
     app.run_server(debug=True)
